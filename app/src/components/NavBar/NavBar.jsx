50955c4213603708ef5aaf16a5f350e791ae5546
Jvsfarias <jvsdf@outlook.com>
2023-02-07 15:15:17 -0300

    conserto do hamburguer

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -1,30 +1,38 @@
 import "./NavBar.css";
 import logo from "../../assets/atlantePng.png";
+import { useState } from "react";
 
 export default function NavBar() {
-  let ul = document.querySelector(".nav ul");
- 
-function menuShow(){
-    if(ul.classList.contains("open")){
-        ul.classList.remove("open");
-    }else{
-        ul.classList.add("open")
-    }
-}
-
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="nav">
       <img src={logo} alt="Logo" className="logo" />
 
-      <div className="menu-btn"><i className="fa fa-bars fa-2x" onClick={menuShow}></i></div>
+      <div className="menu-btn">
+        <i className="fa fa-bars fa-2x" onClick={() => setIsOpen(!isOpen)}></i>
+      </div>
 
-      <ul className="flex text-white gap-8 items-center text-sm">
-        <li><a href="#">INICIO</a></li>
-        <li><a href="#">PLANOS</a></li>
-        <li><a href="#">LOCALICAÇÃO</a></li>
-        <li><a href="#">PROFESSORES</a></li>
-        <li><a href="#">CONTATO</a></li>
+      <ul
+        className={`${
+          isOpen ? "open" : ""
+        } flex text-white gap-8 items-center text-sm`}
+      >
+        <li>
+          <a href="#">INICiO</a>
+        </li>
+        <li>
+          <a href="#">PLANOS</a>
+        </li>
+        <li>
+          <a href="#">LOCALICAÇÃO</a>
+        </li>
+        <li>
+          <a href="#">PROFESSORES</a>
+        </li>
+        <li>
+          <a href="#">CONTATO</a>
+        </li>
       </ul>
     </div>
   );
