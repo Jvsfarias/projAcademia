0caa27b5179f8dc723f7af89f12df5aa657af150
João Victor <jvsdf@outlook.com>
2023-02-07 08:08:12 -0300

    menu hamburguer

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -2,15 +2,29 @@ import "./NavBar.css";
 import logo from "../../assets/atlantePng.png";
 
 export default function NavBar() {
+  let ul = document.querySelector(".nav ul");
+ 
+function menuShow(){
+    if(ul.classList.contains("open")){
+        ul.classList.remove("open");
+    }else{
+        ul.classList.add("open")
+    }
+}
+
+
   return (
     <div className="nav">
       <img src={logo} alt="Logo" className="logo" />
-      <ul className="flex text-white gap-8 items-center text-lg">
-        <li>INICIO</li>
-        <li>PLANOS</li>
-        <li>LOCALIZAÇÃO</li>
-        <li>PROFESSORES</li>
-        <li>CONTATO</li>
+
+      <div className="menu-btn"><i className="fa fa-bars fa-2x" onClick={menuShow}></i></div>
+
+      <ul className="flex text-white gap-8 items-center text-sm">
+        <li><a href="#">INCIO</a></li>
+        <li><a href="#">PLANOS</a></li>
+        <li><a href="#">LOCALICAÇÃO</a></li>
+        <li><a href="#">PROFESSORES</a></li>
+        <li><a href="#">CONTATO</a></li>
       </ul>
     </div>
   );
