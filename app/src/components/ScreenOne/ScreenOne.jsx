3c94ac14be32289b34087014f7becb486da099d2
Jvsfarias <jvsdf@outlook.com>
2023-02-09 14:39:56 -0300

    ajuste marketing

--- app/src/components/ScreenOne/ScreenOne.jsx
+++ app/src/components/ScreenOne/ScreenOne.jsx
@@ -1,4 +1,16 @@
 import "./ScreenOne.css";
 export default function ScreenOne() {
-  return <div className="backgroundScreenOne"></div>;
+  return (
+    <div>
+      <div className="imagem" />
+      <div className="marketing text-white inline-block ">
+        <h1 className="topText">ESCOLHA A</h1>
+        <h1 className="topText">ACADEMIA QUE</h1>
+        <h1 className="lastText">VOCÊ MERECE!</h1>
+        <button className="px-[5rem] py-[2rem] font-black border-orangeProject border-[1px] text-4xl mt-5 hover:scale-105">
+          CONHECER PLANOS
+        </button>
+      </div>
+    </div>
+  );
 }
