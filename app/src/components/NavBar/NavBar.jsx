79d833b6268fa9c797c8a586ea988ff72a76f8ee
Jvsfarias <jvsdf@outlook.com>
2023-02-12 15:28:50 -0300

    tudo certo

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import "./NavBar.css";
 import logo from "../../assets/atlantePng.png";
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import HomeIcon from '@mui/icons-material/Home';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import SchoolIcon from '@mui/icons-material/School';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import HomeIcon from "@mui/icons-material/Home";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import SchoolIcon from "@mui/icons-material/School";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { useState } from "react";
 
 export default function NavBar() {
@@ -21,27 +21,32 @@ export default function NavBar() {
       <ul
         className={`${
           isOpen ? "open" : ""
-        } flex text-white gap-8 items-center text-sm`}
+        } flex text-white gap-8 items-center font-bold text-sm`}
       >
         <li>
-          <HomeIcon className="iconNav"/>
+          <HomeIcon className="iconNav" />
           <a href="#">INICIO</a>
         </li>
         <li>
-          <FitnessCenterIcon className="iconNav"/>
+          <FitnessCenterIcon className="iconNav" />
           <a href="#">PLANOS</a>
         </li>
         <li>
-          <LocationOnIcon className="iconNav"/>
+          <LocationOnIcon className="iconNav" />
           <a href="#">LOCALIZAÇÃO</a>
         </li>
         <li>
-          <SchoolIcon className="iconNav"/>
+          <SchoolIcon className="iconNav" />
           <a href="#">PROFESSORES</a>
         </li>
         <li>
-          <WhatsAppIcon className="iconNav"/>
+          <WhatsAppIcon className="iconNav" />
           <a href="#">CONTATO</a>
+        </li>
+        <li>
+          <a href="#" className="aulaGratis">
+            AULA GRATIS
+          </a>
         </li>
       </ul>
     </div>
--- app/src/components/ScreenOne/ScreenOne.jsx
+++ app/src/components/ScreenOne/ScreenOne.jsx
@@ -7,7 +7,7 @@ export default function ScreenOne() {
         <h1 className="topText">ESCOLHA A</h1>
         <h1 className="topText">ACADEMIA QUE</h1>
         <h1 className="lastText">VOCÊ MERECE!</h1>
-        <button className="px-[5rem] py-[2rem] font-black border-orangeProject border-[1px] text-4xl mt-5 hover:scale-105">
+        <button className="px-[4rem] py-[1.5 rem] font-black border-orangeProject border-[2px] text-3xl mt-5 hover:scale-105">
           CONHECER PLANOS
         </button>
       </div>
