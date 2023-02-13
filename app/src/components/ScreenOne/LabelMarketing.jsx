e922bbb1e31e868032bcb148b7f94e1c9211e6b7
João Victor <jvsdf@outlook.com>
2023-02-13 08:24:49 -0300

    começo screen2

---
+++ app/src/components/ScreenOne/LabelMarketing.jsx
@@ -0,0 +1,10 @@
+import "./LabelMarketing.css"
+export default function LabelMarketing(){
+    return(
+        <div>
+            <h1 className="topText">Escolha a</h1>
+            <h1 className="topText">academia que</h1>
+            <h1 className="lastText">VOCÊ MERECE!</h1>
+        </div>
+    )
+}
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         orangeProject: "#db7119;",
+        grayText: "#5C5C5C;",
       },
     },
   },
