40e455311103f32aaad52d4ab1e70aa21947861b
Jvsfarias <jvsdf@outlook.com>
2023-02-23 08:23:15 -0300

    criacao whatsIcon

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import ScreenOne from "./components/Screen1/ScreenOne";
+import WhatsButton from "./components/NavBar/WhatsButton";
 import ScreenTwo from "./components/Screen2/ScreenTwo";
 import ScreenTree from "./components/Screen3/ScreenTree";
 import ScreenFour from "./components/Screen4/ScreenFour";
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <div className="app">
       <NavBar />
+      <WhatsButton />
       <ScreenOne />
       <ScreenTwo />
       <ScreenTree />
--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -6,16 +6,21 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SchoolIcon from "@mui/icons-material/School";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { useState } from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    
-    <div className="box nav"> 
-      <motion.img src={logo} alt="Logo" className="logo" initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }} transition={{ duration: 0.5 }}/>
+    <div className="box nav">
+      <motion.img
+        src={logo}
+        alt="Logo"
+        className="logo"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      />
 
       <div className="menu-btn">
         <i className="fa fa-bars fa-2x" onClick={() => setIsOpen(!isOpen)}></i>
@@ -26,38 +31,56 @@ export default function NavBar() {
           isOpen ? "open" : ""
         } flex text-white gap-8 items-center font-bold text-sm`}
       >
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.2 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
           <HomeIcon className="iconNav" />
           <a href="#">INICIO</a>
         </motion.li>
 
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.3 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.3 }}
+        >
           <FitnessCenterIcon className="iconNav" />
           <a href="#">PLANOS</a>
         </motion.li>
 
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.4 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.4 }}
+        >
           <LocationOnIcon className="iconNav" />
           <a href="#">LOCALIZAÇÃO</a>
         </motion.li>
 
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.5 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <SchoolIcon className="iconNav" />
-          <a href="#">PROFESSORES</a>
+          <a href="#">SOBRE</a>
         </motion.li>
 
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.6 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
           <WhatsAppIcon className="iconNav" />
           <a href="#">CONTATO</a>
         </motion.li>
 
-        <motion.li initial={{ y: "-110%" }}
-      animate={{ y: 0 }} transition={{ duration: 0.7 }}>
+        <motion.li
+          initial={{ y: "-110%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
           <a href="#" className="aulaGratis">
             AULA GRATIS
           </a>
